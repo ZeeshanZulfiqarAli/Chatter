9025d6854130e5249099a43db705aa3426328c77
Zeeshan <zeeshan.dossani23@gmail.com>
2022-06-18 14:05:26 +0500

    fixed socket url

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { io } from "socket.io-client";
 // import { SOCKET_URL } from "config";
 
-// const SOCKET_URL = window.location.hostname + ':'
-
+const SOCKET_URL = window.location.hostname + ':'
+// const SOCKET_URL = process.env.YOUR_HOST || '0.0.0.0' + ':' + process.env.PORT || 5000;
 // export const socket = io(SOCKET_URL);
-export const socket = io('http://localhost:5000');
+export const socket = io(SOCKET_URL);
 export const SocketContext = React.createContext();
